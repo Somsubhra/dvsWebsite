016ccae7b6e09ff2cf3ddc290f83464af88ad310
somsubhra <somsubhra.bairi@gmail.com>
2012-12-10 22:10:26 +0530

    Commit menubar

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="code/menuStyle.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome: Dang Vikas Sansthan</title>
+</head>
+
+
+<frameset rows="105,*" border="0" frameborder="no">
+<frame src="menu.html" /></frameset>
+
+<noframes>Your browser does not support frames</noframes>
+
+</html>
