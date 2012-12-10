11d0c9f346074307169aa45670d430256c8261eb
somsubhra <somsubhra.bairi@gmail.com>
2012-12-10 22:56:55 +0530

    commit ticker

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link rel="stylesheet" href="code/menuStyle.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Welcome: Dang Vikas Sansthan</title>
 </head>
 
 
 <frameset rows="105,*" border="0" frameborder="no">
-<frame src="menu.html" /></frameset>
+<frame src="menu.html" />
+<frameset cols="1010,*" border="0" frameborder="no">
+<frame src="jdk.html"  />
+<frame src="ticker.html" />
+</frameset>
 
-<noframes>Your browser does not support frames</noframes>
+</frameset>
+
+<noframes>Your browser does not support frames.</noframes>
 
 </html>
