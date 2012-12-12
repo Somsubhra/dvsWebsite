206fbe45583594705ec03b7301a5c602a37f331e
somsubhra <somsubhra.bairi@gmail.com>
2012-12-12 17:02:14 +0530

    latest

--- index.php
+++ index.php
@@ -1,20 +1,56 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<link rel="stylesheet" href="style/menuStyle.css" />
+<link rel="stylesheet" href="style/ticker.css" />
+<script src="script/jquery-1.2.6.min.js" type="text/javascript"></script>
+<script src="script/ticker.js" type="text/javascript"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Welcome: Dang Vikas Sansthan</title>
 </head>
+<body>
+
+<ul id="menu">
+	<li><a href="#">Home</a></li>
+    <li><a href="#">About us</a></li>
+    <li><a href="#">Our Mission</a></li>
+    <li><a href="#">Field Work</a></li>
+    <li><a href="#">Projects</a></li>
+    <li><a href="#">Achievements</a></li>
+    <li><a href="#">Contact Us</a></li>
+    <li><a href="#">Associations</a></li>
+    <li><a href="#">Picture Gallery</a></li>
+</ul>
 
 
-<frameset rows="105,*" border="0" frameborder="no">
-<frame src="menu.html" />
-<frameset cols="1010,*" border="0" frameborder="no">
-<frame src="jdk.html"  />
-<frame src="ticker.html" />
-</frameset>
 
-</frameset>
+<ul id="ticker">
+		<li>
+			<a href="#">This is news1</a>
+		</li>
+		<li>
+			<a href="#">This is news2</a>
+		</li>
+		<li>
+			<a href="#">This is news3</a>
+		</li>
+		<li>
+			<a href="#">This is news4</a>
+		</li>
+        		<li>
+			<a href="#">This is news5</a>
+		</li>
+		<li>
+			<a href="#">This is news6</a>
+		</li>
+		<li>
+			<a href="#">This is news7</a>
+		</li>
+		<li>
+			<a href="#">This is news8</a>
+		</li>
+</ul>
 
-<noframes>Your browser does not support frames.</noframes>
+</body>
 
 </html>
