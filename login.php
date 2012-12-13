aed4f6ff072a0ec9a0749c264a3c226a08187c92
Yash Shah <blazonware@gmail.com>
2012-12-13 10:24:25 +0530

    Indenting login.php

--- login.php
+++ login.php
@@ -14,22 +14,22 @@ $errflag = false;
 //Connect to mysql server
 $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 if(!$link) {
-die('Failed to connect to server: ' . mysql_error());
+  die('Failed to connect to server: ' . mysql_error());
 }
 
 //Select database
 $db = mysql_select_db(DB_DATABASE);
 if(!$db) {
-die("Unable to select database");
+  die("Unable to select database");
 }
 
 //Function to sanitize values received from the form. Prevents SQL injection
 function clean($str) {
-$str = @trim($str);
-if(get_magic_quotes_gpc()) {
-$str = stripslashes($str);
-}
-return mysql_real_escape_string($str);
+  $str = @trim($str);
+  if(get_magic_quotes_gpc()) {
+    $str = stripslashes($str);
+  }
+  return mysql_real_escape_string($str);
 }
 
 //Sanitize the POST values
@@ -38,20 +38,20 @@ $password = clean($_POST['password']);
 
 //Input Validations
 if($username == '') {
-$errmsg_arr[] = 'User name missing';
-$errflag = true;
+  $errmsg_arr[] = 'User name missing';
+  $errflag = true;
 }
 if($password == '') {
-$errmsg_arr[] = 'Password missing';
-$errflag = true;
+  $errmsg_arr[] = 'Password missing';
+  $errflag = true;
 }
 
 //If there are input validations, redirect back to the registration form
 if($errflag) {
-$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
-session_write_close();
-header("location: index.php");
-exit();
+  $_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+  session_write_close();
+  header("location: index.php");
+  exit();
 }
 
 //Create INSERT query
@@ -60,9 +60,9 @@ $result = @mysql_query($query);
 
 //Check whether the query was successful or not
 if($result) {
-header("location: admin.php");
-exit();
+  header("location: admin.php");
+  exit();
 }else {
-die("Query failed");
+  die("Query failed");
 }
 ?>
